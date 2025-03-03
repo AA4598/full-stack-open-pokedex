c6386c3ae45c16d9a5edf9d7ea3cb3c9270fca6c
AA4598 <76053158+AA4598@users.noreply.github.com>
2025-03-04 01:46:42 +0200

    11.9-2

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -8,7 +8,7 @@ describe('Pokedex', function() {
   })
 
   it('page Ivysaur can be opened', function() {
-    cy.visit('http://localhost:5000/pokemon/Ivysaur')
+    cy.visit('http://localhost:8080/pokemon/kakuna')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
