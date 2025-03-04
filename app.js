dfec0632b99babcb586bfc131d2bfe975a5aaf35
AA4598 <76053158+AA4598@users.noreply.github.com>
2025-03-04 12:42:25 +0200

    11.10-3

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
@@ -7,8 +8,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('*', function (request, response) {
-  response.sendFile(path.resolve(__dirname, 'index.html'));
-});
+  response.sendFile(path.resolve(__dirname, 'index.html'))
+})
 
 app.listen(PORT, () => {
   //console.log(`server started on port ${PORT}`)
